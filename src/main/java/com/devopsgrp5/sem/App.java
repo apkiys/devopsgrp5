abb6ed4495f5593c086605661332dbf4f9243d53
Chris <20112494+apkiys@users.noreply.github.com>
2024-10-16 20:15:19 +0800

    Minor update to com.devopsgrp5.sem App v0.1.0.2.

--- src/main/java/com/devopsgrp5/sem/App.java
+++ src/main/java/com/devopsgrp5/sem/App.java
@@ -1,4 +1,4 @@
-package com.napier.sem;
+package com.devopsgrp5.sem;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
@@ -9,7 +9,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB
+        // Connect to MongoDB on local system - we're using port 27000
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
