075f17d7ef7088b1ec34653f95ffbf4632c957f9
Chris <20112494+apkiys@users.noreply.github.com>
2024-12-03 02:47:16 +0800

    Update docker compose

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -1,40 +1,107 @@
 package com.napier.devops;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.io.BufferedWriter;
-import java.io.*;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
+@SpringBootApplication
+@RestController
 public class App {
 
     /**
      * Connection to MySQL database.
      */
-    private Connection con = null;
-    public static void main(String[] args) {
-        // Create new Application
-        App a = new App();
+    private static Connection con = null;
 
+    public static void main(String[] args) throws IOException {
+
+        // Connect to database
         if (args.length < 1) {
-            //local
-            a.connect("localhost:33060", 0);
+            connect("localhost:33060", 0);
         } else {
-            //docker parameters passed from Dockerfile
-            a.connect(args[0], Integer.parseInt(args[1]));
+            //connect via docker or actions
+            connect(args[0], 10000);
+            report2();
+            //needed on actions else build runs forever
+            // changed added dummy container that sleeps for a min then exits so actions works
+            //System.exit(0);
         }
 
-        try {
-            a.report2();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        SpringApplication.run(App2.class, args);
 
-        // Disconnect from database
-        a.disconnect();
     }
 
-    public void report2() throws IOException {
+    @RequestMapping("cities")
+    public ArrayList<City> getCities() {
+        ArrayList<City> cities = new ArrayList<>();
+        try {
+
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String sql = "select * from city";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(sql);
+            //cycle
+            while (rset.next()) {
+                Integer id = rset.getInt("ID");
+                String name = rset.getString("Name");
+                String countryCode = rset.getString("CountryCode");
+                String district = rset.getString("District");
+                Integer population = rset.getInt("Population");
+                City city = new City(id, name, countryCode, district, population);
+                cities.add(city);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get details");
+            return null;
+        }
+        return cities;
+    }
+
+    @RequestMapping("city")
+    public ArrayList<City> getCity(@RequestParam(value = "id") String id) {
+        City city = null;
+        ArrayList<City> cities = new ArrayList<>();
+        try {
+
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String sql = "select * from city where ID = " + id;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(sql);
+            //cycle
+            if (rset.next()) {
+                String name = rset.getString("Name");
+                String countryCode = rset.getString("CountryCode");
+                String district = rset.getString("District");
+                Integer population = rset.getInt("Population");
+                city = new City(Long.parseLong(id), name, countryCode, district, population);
+                cities.add(city);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get details");
+            return null;
+        }
+        System.out.println(city);
+        return cities;
+    }
+
+    public static void report2() {
         StringBuilder sb = new StringBuilder();
         try {
             // Create an SQL statement
@@ -50,8 +117,7 @@ public class App {
                 sb.append(name + "\t" + population + "\r\n");
             }
             new File("./output/").mkdir();
-            BufferedWriter writer = new BufferedWriter(
-                    new FileWriter(new File("./output/report1.txt")));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File("./output/report1.txt")));
             writer.write(sb.toString());
             writer.close();
             System.out.println(sb.toString());
@@ -60,19 +126,15 @@ public class App {
             System.out.println("Failed to get details");
             return;
         }
-
-        System.out.println(sb.toString());
     }
 
     /**
      * Connect to the MySQL database.
      *
-     * @param conString
-     * 		Use db:3306 for docker and localhost:33060 for local or Integration
-     * 		Tests
+     * @param conString Use db:3306 for docker and localhost:33060 for local or Integration Tests
      * @param
      */
-    public void connect(String conString, int delay) {
+    public static void connect(String conString, int delay) {
         try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -90,24 +152,24 @@ public class App {
                 // Connect to database
                 //Added allowPublicKeyRetrieval=true to get Integration Tests
                 // to work. Possibly due to accessing from another class?
-                con = DriverManager.getConnection("jdbc:mysql://" + conString
-                        + "/world?allowPublicKeyRetrieval=true&useSSL"
-                        + "=false", "root", "example");
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://" + conString + "/world?allowPublicKeyRetrieval=true&useSSL" + "=false", "root",
+                        "example");
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt "
-                        + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
     }
+
     /**
      * Disconnect from the MySQL database.
      */
-    public void disconnect() {
+    public static void disconnect() {
         if (con != null) {
             try {
                 // Close connection
@@ -115,6 +177,16 @@ public class App {
             } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+
+    public void printCityReport(ArrayList<City> cities) {
+        if (cities == null) {
+            System.out.println("No cities found");
+            return;
+        }
+        for (City city : cities) {
+            System.out.println(city);
         }
     }
 }
---
+++ src/main/java/com/napier/devops/City.java
@@ -0,0 +1,48 @@
+package com.napier.devops;
+
+/**
+ * Object representation of the City Table from World database
+ */
+public class City {
+    private long id;
+    private String name;
+    private String countryCode;
+    private String district;
+    private long population;
+
+    public City(long id, String name, String countryCode, String district,
+                long population) {
+        this.id = id;
+        this.name = name;
+        this.countryCode = countryCode;
+        this.district = district;
+        this.population = population;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" + "id=" + id + ", name='" + name + '\''
+                + ", countryCode='" + countryCode + '\'' + ", district='"
+                + district + '\'' + ", population=" + population + '}';
+    }
+}
